720ec2a80b14a09b9d67ebc753a2a5b7c5a38ea9
MartinOtamendiT <martin.otamendit@alumno.buap.mx>
2024-04-15 22:49:08 -0600

    Add "Editar Admins y Maestros"
    Co-authored-by: Brandon Lopez <BrandLop@users.noreply.github.com>

--- computacion_api/urls.py
+++ computacion_api/urls.py
@@ -26,14 +26,20 @@ urlpatterns = [
         path('admin/', users.AdminView.as_view()),
     #Admin Data
         path('lista-admins/',users.AdminAll.as_view()),
+    #Edit Admin
+        path('admins-edit/', users.AdminsViewEdit.as_view()),
     #Create Alumno
         path('alumno/', alumnos.AlumnoView.as_view()),
     #Alumno Data
         path('lista-alumnos/', alumnos.AlumnosAll.as_view()),
+    #Edit Alumno
+        #path('alumnos-edit/', alumnos.AlumnosViewEdit.as_view()),
     #Create Maestro
         path('maestro/', maestros.MaestroView.as_view()),
     #Maestro Data
         path('lista-maestros/',maestros.MaestrosAll.as_view()),
+    #Edit Maestro
+        path('maestros-edit/', maestros.MaestrosViewEdit.as_view()),
     #Login
         path('token/', auth.CustomAuthToken.as_view()),
     #Logout
